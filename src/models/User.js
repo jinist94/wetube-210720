01bc1c6cd13efc3525aff33b6e99f95362f3435c
jinist94 <jay@Hamui-MacBook-Pro.local>
2021-08-09 00:33:30 +0900

    user

--- src/models/User.js
+++ src/models/User.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
-    email : { type: String, requered:true, unique: true},
-    username : {type: String, requered:true, unique: true},
-    password : {type: String, requered:true, unique: true},
-    name: {type: String, requered:true, unique: true},
+    email : { type: String, required:true, unique: true},
+    username : {type: String, required:true, unique: true},
+    password : {type: String, required:true, unique: true},
+    name: {type: String, required:true, unique: true},
     location : String
 })
 
