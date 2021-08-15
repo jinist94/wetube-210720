0164f4f38807d2f4c06e54e4356a84c6d508812f
jinist94 <jay@Hamui-MacBook-Pro.local>
2021-08-16 04:32:20 +0900

    github login
    ing

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
+import fetch from "node-fetch";
 
 export const getJoin = (req, res) => res.render("Join", {pageTitle: "Join"});
 export const postJoin = async(req, res) => {
@@ -56,16 +57,44 @@ export const postLogin = async(req, res) => {
 export const startGithubLogin = (req, res) => {
     const baseUrl = "https://github.com/login/oauth/authorize";
     const config = {
-        client_id:"893b664abf83f2c71af0",
+        client_id: process.env.GH_CLIENT,
         allow_signup:false,
-        scope : "read:user user:email" //should be saperated by space  
+        scope: "read:user user:email",  
     }
     const params = new URLSearchParams(config).toString();
     const finalUrl = `${baseUrl}?${params}`;
-    return res.redirect(finalUrl)
+    return res.redirect(finalUrl);
 }
-export const finishGithubLogin = (req, res) => {
-
+export const finishGithubLogin = async(req, res) => {
+    const baseUrl = "https://github.com/login/oauth/access_token";
+    const config ={
+        client_id: process.env.GH_CLIENT,
+        client_secret: process.env.GH_SECRET,
+        code: req.query.code,
+    }
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    const tokenRequest = await (
+        await fetch(finalUrl, {
+        method:"POST",
+        headers: {
+            Accept: "application/json",
+        },
+        })
+        ).json();
+    if("access_token" in tokenRequest) {
+        const {access_token} = tokenRequest;
+        const userRequest = await(
+            await fetch("https://api.github.com/user", {
+            headers:{
+                Authorization: `token ${access_token}`,
+            }
+        })
+        ).json();
+        console.log(userRequest)
+    }else {
+        return res.redirect("/");
+    }
 }
 
 export const getEdit = (req, res) => {
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,7 +7,7 @@ const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
 userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(uploadAvatar.single("avatar"), postEdit);
-userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 userRouter.get("/:id", see);
