34404c563271b8b8442532b17b3cb40cd9a2c94c
jinist94 <jay@Hamui-MacBook-Pro.local>
2021-08-17 03:19:14 +0900

    videoPlayer setting

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -3,10 +3,14 @@ console.log("Video Player");
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreen");
+const videoContainer = document.getElementById("videoContainer");
 
-console.log(play,mute,time,volume);
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -44,6 +48,41 @@ const handleVolumeChange = (event) => {
     video.volume = value;
 
 }
+
+const formatTime = (seconds) => 
+    new Date(seconds*1000).toISOString().substr(14,5);
+
+
+const handleLoadedMetadata = () => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
+}
+const handleTimeUpdate = () => {
+    currentTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
+}
+
+const handleTimelineChange = (event) =>{
+    const {target: {value} } = event;
+    video.currentTime = value;
+}
+
+const handleFullscreen = () => {
+    const fullscreen = document.fullscreenElement;
+    if(fullscreen) {
+        document.exitFullscreen();
+        fullScreenBtn.innerText= "Enter Full Screen";
+    }else {
+
+        videoContainer.requestFullscreen();
+        fullScreenBtn.innerText= "Exit Full Screen";
+    }
+}
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimelineChange);
+fullScreenBtn.addEventListener("click", handleFullscreen);
