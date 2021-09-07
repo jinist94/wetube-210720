38eda81589c19ef986ad179acccef83644583818
jinist94 <jay@Hamui-MacBook-Pro.local>
2021-09-08 01:47:05 +0900

    POST env variable

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListener = () => console.log(`✅Server Listening on ${PORT}❗️`);
 app.listen(PORT, handleListener);
